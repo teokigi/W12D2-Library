915393d8933532c73fc12e486820c1bdbf49f322
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-26 15:52:55 +0100

    BookTest
    Added some classes, finished BookTest

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookTest {
+
+    Book book;
+
+    @Before
+    public void before(){
+        book = new Book("title","author","genre");
+    }
+
+    @Test
+    public void hasTitle(){
+        assertEquals("title",book.getTitle());
+    }
+
+    @Test
+    public void hasAuthor(){
+        assertEquals("author",book.getAuthor());
+    }
+
+    @Test
+    public void hasGenre(){
+        assertEquals("genre", book.getGenre());
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+
+public class LibraryTest {
+
+    Library library;
+
+    @Before
+    public void before(){
+        library = new Library();
+    }
+}
